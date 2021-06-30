96d1f2ec3c4b13bd9fb27fe7d6b2129570febfe5
Leandro Lara <leandrodasilvalara@yahoo.com.br>
2021-06-30 18:12:21 -0300

    Adicionando o FloatingActionButton

---
+++ lib/home_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("NOTES"),
+        centerTitle: true,
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          print("CLICOU");
+        },
+      ),
+    );
+    
+  }
+
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:notes/app_widget.dart';
 
 void main() {
-  runApp(Container(
-    color: Colors.purple,
+  runApp(AppWidget(
+
   ));
 }
